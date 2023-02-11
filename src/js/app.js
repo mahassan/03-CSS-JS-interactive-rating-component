0fa8c080d8f21cbd505a6d354ab466e29b2b9bc0
Ammar Hassan <ammar@ammarhassan.co.uk>
2023-02-11 14:07:00 +0000

    formData not working

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,7 @@
 const form = document.querySelector("#rating")
 form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    const data = new FormData(form);
+    const rating = this.choice.value;
     
-    [...form].forEach((el)=>{
-        if(el.checked){
-            localStorage.setItem("rating",el.value)
-            window.location.href = "http://127.0.0.1:5500/src/thank-you.html";
-        }else{
-            alert("Please select a rating, it helps us greatly!")
-            return
-        }
-    })
+    // debugging
+    console.log(`chosen value was ${rating}`);
 })
