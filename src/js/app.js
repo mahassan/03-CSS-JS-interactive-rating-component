3f62c5d75bd14db3da36097ff2aa23ed5d62b7a7
Ammar Hassan <ammar@ammarhassan.co.uk>
2023-02-12 19:47:03 +0000

    working rating component

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("#rating")
 form.addEventListener("submit", function(e){
+    const rating = this.ratings.value
     // debugging
     console.log(`chosen value was ${rating}`);
 })
